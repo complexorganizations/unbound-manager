7f3e1aa065f4213aa31b08df97187f3871fbb507
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-02-26 22:59:01 -0500

    Update unbound-manager.sh

--- unbound-manager.sh
+++ unbound-manager.sh
@@ -3,7 +3,7 @@
 
 # Require script to be run as root
 function super-user-check() {
-  if [ "$EUID" -ne 0 ]; then
+  if [ "${EUID}" -ne 0 ]; then
     echo "You need to run this script as super user."
     exit
   fi
@@ -17,7 +17,7 @@ function dist-check() {
   if [ -e /etc/os-release ]; then
     # shellcheck disable=SC1091
     source /etc/os-release
-    DISTRO=$ID
+    DISTRO=${ID}
   fi
 }
 
@@ -26,22 +26,22 @@ dist-check
 
 # Pre-Checks system requirements
 function installing-system-requirements() {
-  if { [ "$DISTRO" == "ubuntu" ] || [ "$DISTRO" == "debian" ] || [ "$DISTRO" == "raspbian" ] || [ "$DISTRO" == "pop" ] || [ "$DISTRO" == "kali" ] || [ "$DISTRO" == "linuxmint" ] || [ "$DISTRO" == "fedora" ] || [ "$DISTRO" == "centos" ] || [ "$DISTRO" == "rhel" ] || [ "$DISTRO" == "arch" ] || [ "$DISTRO" == "manjaro" ] || [ "$DISTRO" == "alpine" ] || [ "$DISTRO" == "freebsd" ]; }; then
+  if { [ "${DISTRO}" == "ubuntu" ] || [ "${DISTRO}" == "debian" ] || [ "${DISTRO}" == "raspbian" ] || [ "${DISTRO}" == "pop" ] || [ "${DISTRO}" == "kali" ] || [ "${DISTRO}" == "linuxmint" ] || [ "${DISTRO}" == "fedora" ] || [ "${DISTRO}" == "centos" ] || [ "${DISTRO}" == "rhel" ] || [ "${DISTRO}" == "arch" ] || [ "${DISTRO}" == "manjaro" ] || [ "${DISTRO}" == "alpine" ] || [ "${DISTRO}" == "freebsd" ]; }; then
     if [ ! -x "$(command -v curl)" ]; then
-      if { [ "$DISTRO" == "ubuntu" ] || [ "$DISTRO" == "debian" ] || [ "$DISTRO" == "raspbian" ] || [ "$DISTRO" == "pop" ] || [ "$DISTRO" == "kali" ] || [ "$DISTRO" == "linuxmint" ]; }; then
+      if { [ "${DISTRO}" == "ubuntu" ] || [ "${DISTRO}" == "debian" ] || [ "${DISTRO}" == "raspbian" ] || [ "${DISTRO}" == "pop" ] || [ "${DISTRO}" == "kali" ] || [ "${DISTRO}" == "linuxmint" ]; }; then
         apt-get update && apt-get install curl -y
-      elif { [ "$DISTRO" == "fedora" ] || [ "$DISTRO" == "centos" ] || [ "$DISTRO" == "rhel" ]; }; then
+      elif { [ "${DISTRO}" == "fedora" ] || [ "${DISTRO}" == "centos" ] || [ "${DISTRO}" == "rhel" ]; }; then
         yum update -y && yum install curl -y
-      elif { [ "$DISTRO" == "arch" ] || [ "$DISTRO" == "manjaro" ]; }; then
+      elif { [ "${DISTRO}" == "arch" ] || [ "${DISTRO}" == "manjaro" ]; }; then
         pacman -Syu && pacman -Syu --noconfirm curl
-      elif [ "$DISTRO" == "alpine" ]; then
+      elif [ "${DISTRO}" == "alpine" ]; then
         apk update && apk add curl
-      elif [ "$DISTRO" == "freebsd" ]; then
+      elif [ "${DISTRO}" == "freebsd" ]; then
         pkg update && pkg install curl
       fi
     fi
   else
-    echo "Error: $DISTRO not supported."
+    echo "Error: ${DISTRO} not supported."
     exit
   fi
 }
@@ -53,18 +53,18 @@ installing-system-requirements
 RESOLV_CONFIG="/etc/resolv.conf"
 RESOLV_CONFIG_OLD="/etc/resolv.conf.old"
 UNBOUND_ROOT="/etc/unbound"
-UNBOUND_MANAGER="$UNBOUND_ROOT/unbound-manager"
-UNBOUND_CONFIG="$UNBOUND_ROOT/unbound.conf"
-UNBOUND_ROOT_HINTS="$UNBOUND_ROOT/root.hints"
+UNBOUND_MANAGER="${UNBOUND_ROOT}/unbound-manager"
+UNBOUND_CONFIG="${UNBOUND_ROOT}/unbound.conf"
+UNBOUND_ROOT_HINTS="${UNBOUND_ROOT}/root.hints"
 UNBOUND_ANCHOR="/var/lib/unbound/root.key"
 UNBOUND_ROOT_SERVER_CONFIG_URL="https://www.internic.net/domain/named.cache"
 UNBOUND_MANAGER_UPDATE_URL="https://raw.githubusercontent.com/complexorganizations/unbound-manager/main/unbound-manager.sh"
 
-if [ ! -f "$UNBOUND_MANAGER" ]; then
+if [ ! -f "${UNBOUND_MANAGER}" ]; then
 
   # Function to install unbound
   function install-unbound() {
-    if [ "$DISTRO" == "ubuntu" ]; then
+    if [ "${DISTRO}" == "ubuntu" ]; then
       apt-get install unbound unbound-host e2fsprogs -y
       if pgrep systemd-journal; then
         systemctl stop systemd-resolved
@@ -73,30 +73,30 @@ if [ ! -f "$UNBOUND_MANAGER" ]; then
         service systemd-resolved stop
         service systemd-resolved disable
       fi
-    elif { [ "$DISTRO" == "debian" ] || [ "$DISTRO" == "raspbian" ] || [ "$DISTRO" == "pop" ] || [ "$DISTRO" == "kali" ] || [ "$DISTRO" == "linuxmint" ]; }; then
+    elif { [ "${DISTRO}" == "debian" ] || [ "${DISTRO}" == "raspbian" ] || [ "${DISTRO}" == "pop" ] || [ "${DISTRO}" == "kali" ] || [ "${DISTRO}" == "linuxmint" ]; }; then
       apt-get install unbound unbound-host e2fsprogs -y
-    elif { [ "$DISTRO" == "centos" ] || [ "$DISTRO" == "rhel" ]; }; then
+    elif { [ "${DISTRO}" == "centos" ] || [ "${DISTRO}" == "rhel" ]; }; then
       yum install unbound unbound-libs -y
-    elif [ "$DISTRO" == "fedora" ]; then
+    elif [ "${DISTRO}" == "fedora" ]; then
       dnf install unbound -y
-    elif { [ "$DISTRO" == "arch" ] || [ "$DISTRO" == "manjaro" ]; }; then
+    elif { [ "${DISTRO}" == "arch" ] || [ "${DISTRO}" == "manjaro" ]; }; then
       pacman -Syu --noconfirm unbound
-    elif [ "$DISTRO" == "alpine" ]; then
+    elif [ "${DISTRO}" == "alpine" ]; then
       apk add unbound
-    elif [ "$DISTRO" == "freebsd" ]; then
+    elif [ "${DISTRO}" == "freebsd" ]; then
       pkg install unbound
     fi
-    rm -f $UNBOUND_ANCHOR
-    rm -f $UNBOUND_CONFIG
-    rm -f $UNBOUND_ROOT_HINTS
-    unbound-anchor -a $UNBOUND_ANCHOR
-    curl $UNBOUND_ROOT_SERVER_CONFIG_URL --create-dirs -o $UNBOUND_ROOT_HINTS
+    rm -f ${UNBOUND_ANCHOR}
+    rm -f ${UNBOUND_CONFIG}
+    rm -f ${UNBOUND_ROOT_HINTS}
+    unbound-anchor -a ${UNBOUND_ANCHOR}
+    curl ${UNBOUND_ROOT_SERVER_CONFIG_URL} --create-dirs -o ${UNBOUND_ROOT_HINTS}
     NPROC=$(nproc)
     echo "server:
     num-threads: $NPROC
     verbosity: 1
-    root-hints: $UNBOUND_ROOT_HINTS
-    auto-trust-anchor-file: $UNBOUND_ANCHOR
+    root-hints: ${UNBOUND_ROOT_HINTS}
+    auto-trust-anchor-file: ${UNBOUND_ANCHOR}
     interface: 0.0.0.0
     interface: ::0
     max-udp-size: 3072
@@ -119,13 +119,13 @@ forward-zone:
   forward-addr: 8.8.8.8
   forward-addr: 8.8.4.4
   forward-addr: 2001:4860:4860::8888
-  forward-addr: 2001:4860:4860::8844" >>$UNBOUND_CONFIG
+  forward-addr: 2001:4860:4860::8844" >>${UNBOUND_CONFIG}
     # Set DNS Root Servers
-    chattr -i $RESOLV_CONFIG
-    mv $RESOLV_CONFIG $RESOLV_CONFIG_OLD
-    echo "nameserver 127.0.0.1" >>$RESOLV_CONFIG
-    echo "nameserver ::1" >>$RESOLV_CONFIG
-    chattr +i $RESOLV_CONFIG
+    chattr -i ${RESOLV_CONFIG}
+    mv ${RESOLV_CONFIG} ${RESOLV_CONFIG_OLD}
+    echo "nameserver 127.0.0.1" >>${RESOLV_CONFIG}
+    echo "nameserver ::1" >>${RESOLV_CONFIG}
+    chattr +i ${RESOLV_CONFIG}
     # restart unbound
     if pgrep systemd-journal; then
       systemctl enable unbound
@@ -141,9 +141,9 @@ forward-zone:
   
   # Install unbound manager
   function install-unbound-manager-file() {
-    if [ -d "$UNBOUND_ROOT" ]; then
-      if [ ! -f "$UNBOUND_MANAGER" ]; then
-        echo "Unbound Manager: true" >>$UNBOUND_MANAGER
+    if [ -d "${UNBOUND_ROOT}" ]; then
+      if [ ! -f "${UNBOUND_MANAGER}" ]; then
+        echo "Unbound Manager: true" >>${UNBOUND_MANAGER}
       fi
     fi
   }
@@ -187,7 +187,7 @@ else
       fi
       ;;
     4)
-      if [ -f "$UNBOUND_MANAGER" ]; then
+      if [ -f "${UNBOUND_MANAGER}" ]; then
         if pgrep systemd-journal; then
           systemctl disable unbound
           systemctl stop unbound
@@ -196,35 +196,35 @@ else
           service unbound stop
         fi
         # Change to defualt dns
-        chattr -i $RESOLV_CONFIG
-        rm -f $RESOLV_CONFIG
-        mv $RESOLV_CONFIG_OLD $RESOLV_CONFIG
-        chattr +i $RESOLV_CONFIG
-        if { [ "$DISTRO" == "centos" ] || [ "$DISTRO" == "rhel" ]; }; then
+        chattr -i ${RESOLV_CONFIG}
+        rm -f ${RESOLV_CONFIG}
+        mv ${RESOLV_CONFIG_OLD} ${RESOLV_CONFIG}
+        chattr +i ${RESOLV_CONFIG}
+        if { [ "${DISTRO}" == "centos" ] || [ "${DISTRO}" == "rhel" ]; }; then
           yum remove unbound unbound-host -y
-        elif { [ "$DISTRO" == "debian" ] || [ "$DISTRO" == "pop" ] || [ "$DISTRO" == "ubuntu" ] || [ "$DISTRO" == "raspbian" ] || [ "$DISTRO" == "kali" ] || [ "$DISTRO" == "linuxmint" ]; }; then
+        elif { [ "${DISTRO}" == "debian" ] || [ "${DISTRO}" == "pop" ] || [ "${DISTRO}" == "ubuntu" ] || [ "${DISTRO}" == "raspbian" ] || [ "${DISTRO}" == "kali" ] || [ "${DISTRO}" == "linuxmint" ]; }; then
           apt-get remove --purge unbound unbound-host -y
-        elif { [ "$DISTRO" == "arch" ] || [ "$DISTRO" == "manjaro" ]; }; then
+        elif { [ "${DISTRO}" == "arch" ] || [ "${DISTRO}" == "manjaro" ]; }; then
           pacman -Rs unbound unbound-host -y
-        elif [ "$DISTRO" == "fedora" ]; then
+        elif [ "${DISTRO}" == "fedora" ]; then
           dnf remove unbound -y
-        elif [ "$DISTRO" == "alpine" ]; then
+        elif [ "${DISTRO}" == "alpine" ]; then
           apk del unbound
-        elif [ "$DISTRO" == "freebsd" ]; then
+        elif [ "${DISTRO}" == "freebsd" ]; then
           pkg delete unbound
         fi
-        rm -f $UNBOUND_MANAGER
-        rm -f $UNBOUND_CONFIG
-        rm -f $UNBOUND_ANCHOR
-        rm -f $UNBOUND_ROOT_HINTS
-        rm -f $UNBOUND_ROOT
+        rm -f ${UNBOUND_MANAGER}
+        rm -f ${UNBOUND_CONFIG}
+        rm -f ${UNBOUND_ANCHOR}
+        rm -f ${UNBOUND_ROOT_HINTS}
+        rm -f ${UNBOUND_ROOT}
       fi
       ;;
     5)
       CURRENT_FILE_PATH="$(realpath "$0")"
-      if [ -f "$CURRENT_FILE_PATH" ]; then
-        curl -o "$CURRENT_FILE_PATH" $UNBOUND_MANAGER_UPDATE_URL
-        chmod +x "$CURRENT_FILE_PATH" || exit
+      if [ -f "${CURRENT_FILE_PATH}" ]; then
+        curl -o "${CURRENT_FILE_PATH}" ${UNBOUND_MANAGER_UPDATE_URL}
+        chmod +x "${CURRENT_FILE_PATH}" || exit
       fi
       ;;
     esac
