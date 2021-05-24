22425462d47bea0e2608b04b5a98cc1aee4a8359
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-05-23 23:00:44 -0400

    Update main.go

--- main.go
+++ main.go
@@ -20,7 +20,11 @@ var (
 )
 
 func main() {
-	response, err := http.Get("https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts")
+	validateAndSave("https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts", adwareConfig)
+}
+
+func validateAndSave(url, path string) {
+	response, err := http.Get(url)
 	handleErrors(err)
 	defer response.Body.Close()
 	body, err := io.ReadAll(response.Body)
@@ -29,13 +33,12 @@ func main() {
 	domains := regex.FindAllString(string(body), -1)
 	for _, domain := range domains {
 		if validateDomain(domain) {
-			filePath, err := os.OpenFile(adwareConfig, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+			filePath, err := os.OpenFile(path, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 			handleErrors(err)
 			defer filePath.Close()
 			fileContent := fmt.Sprint(domain, "\n")
 			_, err = filePath.WriteString(fileContent)
 			handleErrors(err)
-			log.Println(fileContent)
 		}
 	}
 }
