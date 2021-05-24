b30819ff4d33fad974b2d1f7bc44a41874313dae
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-05-23 22:41:43 -0400

    Update main.go

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"net"
 	"net/http"
 	"os"
 	"regexp"
@@ -25,16 +26,23 @@ func main() {
 	body, err := io.ReadAll(response.Body)
 	handleErrors(err)
 	regex := regexp.MustCompile(`(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.)+[a-z0-9][a-z0-9-]{0,61}[a-z0-9]`)
-	comments := regex.FindAllString(string(body), -1)
-	for _, comment := range comments {
-		filePath, err := os.OpenFile("file-name", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-		handleErrors(err)
-		defer filePath.Close()
-		fileContent := fmt.Sprint(comment, "\n")
-		_, err = filePath.WriteString(fileContent)
-		handleErrors(err)
-		log.Println(comment)
+	domains := regex.FindAllString(string(body), -1)
+	for _, domain := range domains {
+		if validateDomain(domain) {
+			filePath, err := os.OpenFile(adwareConfig, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+			handleErrors(err)
+			defer filePath.Close()
+			fileContent := fmt.Sprint(domain, "\n")
+			_, err = filePath.WriteString(fileContent)
+			handleErrors(err)
+			log.Println(comment)
+		}
 	}
+}
+
+func validateDomain(domain string) bool {
+	ns, _ := net.LookupNS(domain)
+	return len(ns) >= 1
 }
 
 func handleErrors(err error) {
